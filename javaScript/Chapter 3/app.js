a0b845b7a623c82c49104d0e5ea23fefa34bbac1
aaryan <aaryan.chauhan.1007@gmail.com>
2023-07-28 11:07:33 +0530

    chapter 3 of js

--- javaScript/Chapter 3/app.js
+++ javaScript/Chapter 3/app.js
@@ -11,3 +11,23 @@ console.log(name.replace("aaryan", "chauhan"))
 let students = ["aaryan","marjana","javed","farhana"]
 students[1]="salma"
 students[5]="aniq"
+
+let followers= ["aaryan","soham", "mit", "amin", "umar", "nehan", "danish", "xyz"]
+let blocked = followers.pop()
+followers.unshift("farhana")
+followers.shift()
+
+//practice quesction
+let start = ["january", "july","march","august"];
+start.shift();
+start.shift();
+start.unshift("july");
+start.unshift("june");
+
+start.includes("january")
+start.indexOf("january")
+
+//concatenation
+let primary = ["red","blue","green"]
+let secondary=["violet","indigo","orange"]
+let allColors= primary.concat(secondary)
