0b318fcd4a02e40b25f3c288325492f42ed33d52
aaryan <aaryan.chauhan.1007@gmail.com>
2023-09-10 21:17:34 +0530

    javascript code with harry course add on

--- JavaScript practice/app.js
+++ JavaScript practice/app.js
@@ -44,3 +44,44 @@ const dict = {
 }
 console.log(dict.sartorial);
 console.log(dict["grandiloquent"]);
+
+//operators in js
+console.log("Arithmetic operatrs in JavaScript");
+let A = 10;
+
+console.log("++A=" , ++A);
+    console.log("A++=" , A++);
+    
+    console.log("--A=" , --A);
+    console.log("A--=" , A--);
+    console.log("A=" , A);
+    console.log("A++=" , A++);
+
+//Assignment operators in js
+console.log("Assignment operatrs in JavaScript");
+let assignment=2;
+console.log(assignment);
+console.log("assignment + 5 =", assignment +=5);
+console.log("assignment - 1 =", assignment -=1);
+console.log("assignment * 5 =", assignment *=5);
+console.log("assignment / 2 =", assignment /=2);
+
+//conditional statement
+let age = prompt("pls enter you age");
+age=parseInt(age)
+if (age<0) {
+    console.log("this is invalid age")
+}else if(age<15){
+    console.log("you can't think of start learning to drive")
+}
+else if(age>=15&& age<18){
+    console.log("you can think of start learning to drive")
+}
+else if(age>=18){
+    console.log("you can drive")
+}
+
+//eranary oprater
+let num = prompt("enter your age")
+num=parseInt(num)
+console.log("you can " ,num>18? "drive":"not drive" );
