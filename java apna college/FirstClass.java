83c8f6eba74157a0976ab7df518c63a732868f94
aaryan <aaryan.chauhan.1007@gmail.com>
2023-09-16 00:10:01 +0530

    java start

--- java apna college/FirstClass.java
+++ java apna college/FirstClass.java
@@ -1,5 +1,17 @@
 class FirstClass{
-    public static void main(sting args[]){
-        system.out.println("hello world")
+    public static void main(string args[]) {
+        system.out.println("hello world");
+        public class Main {
+    public static void main(String[] args) {
+        System.out.println("The sum of 3 numbers");
+        int num1 = 2;
+        int num2 = 3;
+        int num3 = 4;
+        int sum =num1 + num2 + num3;
+        System.out.println(sum);
+        System.out.println(num1+num2+num3);
+        }
+    }
+
     }
 }
